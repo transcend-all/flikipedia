bc78e44504c0ced1f039e74c79425ddc0f1c24cc
transcend-all <transcendentalguy@gmail.com>
2017-02-25 14:54:11 -0500

    slight change

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  attr_accessible :email, :password, :password_confirmation
 
 end
